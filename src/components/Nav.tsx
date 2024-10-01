2612b6dbf0a63dafe40ab89da3c329f67b77b6b6
ankitsensei <main.ankitbhagat@gmail.com>
2024-10-01 19:46:42 +0530

    lawyer photo and few intro

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,15 +2,27 @@ import React from 'react'
 import Logo from '../assets/imgs/law_logo.jpg'
 const navbar: React.FC = () => {
     return (
-        <div className="flex mt-5 mx-5 justify-between items-center">
-            <img src={Logo} alt="logo" className='w-12 h-12 rounded-[50%]' />
-            <ul className='flex gap-5'>
-                <li className='navList'>Home</li>
-                <li className='navList'>About</li>
-                <li className='navList'>Services</li>
-                <li className='navList'>Contact</li>
-                <li className='navList bg-red-600 hover:bg-red-700'>Hire Me</li>
-            </ul>
+        <div>
+            <div className='bg-slate-800'>
+            <div className="flex mt-5 mx-5 justify-between items-center">
+                <img src={Logo} alt="logo" className='w-12 h-12 rounded-[50%]' />
+                <ul className='flex gap-5'>
+                    <li className='navList'>Home</li>
+                    <li className='navList'>About</li>
+                    <li className='navList'>Services</li>
+                    <li className='navList'>Contact</li>
+                    <li className='navList bg-red-600 hover:bg-red-700'>Hire Me</li>
+                </ul>
+            </div>
+            <div className='flex justify-between items-center mx-20 mt-40'>
+            <div className="intro flex flex-col items-start">
+                <h1 className='text-5xl'>Hey there,</h1>    
+                <p className='text-5xl'>You may know me as</p>
+                <h1 className='text-8xl font-semibold text-rose-600'>JITENDRA MONDAL</h1>
+            </div>
+                <img src="https://www.pngmart.com/files/5/Lawyer-PNG-Pic.png" alt="lawyer" className="w-auto h-[80vh]" />
+            </div>
+            </div>
         </div>
     )
 }
