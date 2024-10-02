1f15595951fef821086293f322bb4ba587d3aeb2
ankitsensei <main.ankitbhagat@gmail.com>
2024-10-02 23:41:39 +0530

    moved page 1 from navbar page to app page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,17 @@ import NavBar from './components/Nav'
 const App: React.FC = () => {
   return (
     <div>
-      <div className="content">
+      <div id="page1">
         <NavBar/>
+        <div className='flex justify-between items-center mx-[10vw] mt-40'>
+                <div className="intro flex flex-col items-start">
+                    <p className='text-5xl'>You may know me as</p>
+                    <h1 className='text-8xl font-semibold text-rose-600'>JITENDRA MONDAL</h1>
+                    <p className='text-2xl'>Expert Legal Services for Your Needs.</p>
+                    <button className='bg-rose-600 px-10 py-3 rounded-[50px] mt-10 hover:bg-rose-700'>Hire me now</button>
+                </div>
+                    <img src="https://www.pngmart.com/files/5/Lawyer-PNG-Pic.png" alt="lawyer" className="w-auto h-[80vh]" />
+                </div>
       </div>
     </div>
   )
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,6 @@ import Logo from '../assets/imgs/law_logo.jpg'
 const navbar: React.FC = () => {
     return (
         <div>
-            <div className='bg-slate-900'>
             <div className="flex pt-5 px-5 justify-between items-center">
                 <img src={Logo} alt="logo" className='w-12 h-12 rounded-[50%]' />
                 <ul className='flex gap-5'>
@@ -13,14 +12,6 @@ const navbar: React.FC = () => {
                     <li className='navList'>Contact</li>
                     <li className='navList bg-red-600 hover:bg-red-700'>Hire Me</li>
                 </ul>
-            </div>
-            <div className='flex justify-between items-center mx-[10vw] mt-40'>
-                <div className="intro flex flex-col items-start">
-                    <p className='text-5xl'>You may know me as</p>
-                    <h1 className='text-8xl font-semibold text-rose-600'>JITENDRA MONDAL</h1>
-                </div>
-                    <img src="https://www.pngmart.com/files/5/Lawyer-PNG-Pic.png" alt="lawyer" className="w-auto h-[80vh]" />
-                </div>
             </div>
         </div>
     )
